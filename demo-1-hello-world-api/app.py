33f1a058ec7e94d27d55054140524c44e495f2ae
vivek2662 <72120042+vivek2662@users.noreply.github.com>
2023-12-01 14:39:52 +0530

    Update app.py

--- demo-1-hello-world-api/app.py
+++ demo-1-hello-world-api/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def hello_world():
-    return jsonify(message="hello world"), 200
+    return jsonify(message="hello world welome venkat"), 200
 
 
 if __name__ == '__main__':
